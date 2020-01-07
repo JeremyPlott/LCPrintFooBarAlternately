904c55922a57bab4aa57891f8e0ceaadef5940d0
jeremy_plott <plott.j@pg.com>
2020-01-07 10:47:49 -0500

    Add project files.

---
+++ LCPrintFooBarAlternately/Program.cs
@@ -0,0 +1,43 @@
+﻿public class FooBar
+{
+    private int n;
+
+    public FooBar(int n)
+    {
+        this.n = n;
+    }
+
+    bool flag = false;
+
+    public void Foo(Action printFoo)
+    {
+
+        for (int i = 0; i < n; i++)
+        {
+
+            while (flag)
+            {
+                System.Threading.Thread.Sleep(1);
+            }
+            // printFoo() outputs "foo". Do not change or remove this line.
+            printFoo();
+            flag = true;
+        }
+    }
+
+    public void Bar(Action printBar)
+    {
+
+        for (int i = 0; i < n; i++)
+        {
+
+            while (!flag)
+            {
+                System.Threading.Thread.Sleep(1);
+            }
+            // printBar() outputs "bar". Do not change or remove this line.
+            printBar();
+            flag = false;
+        }
+    }
+}
